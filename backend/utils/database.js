9c5fa9a4e823ee26c7f0231ac53ea3e1964e4ef0
Mukul Kumar <149408801+mukulkumar8285@users.noreply.github.com>
2024-06-18 16:59:31 +0530

    Update database.js

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 
 const databaseConnection = async() =>{
-  await mongoose.connect("mongodb+srv://mukulved07:e-commerce@cluster0.1aftvbc.mongodb.net/test").then(()=>{
+  await mongoose.connect("mongodb+srv://mukulved07:e-commerce@cluster0.1aftvbc.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log("MongoDb Connection Successfully")
 
    }).catch((error)=>{
