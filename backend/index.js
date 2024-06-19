e9977336c19df0b0841a7e1709fc9d1d6e0e089b
Mukul Kumar <149408801+mukulkumar8285@users.noreply.github.com>
2024-06-19 11:57:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(cors(corsOption));
 app.options("*", cors(corsOption));
 
 // API routes
-app.use("https://net-flix-full-project.vercel.app/api/v1/user", userRouter);
+app.use("api/v1/user", userRouter);
 app.get("/",(req,res)=>{
   res.json({
     massage:"working",
