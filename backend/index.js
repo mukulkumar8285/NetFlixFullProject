6e5f54c910630618f5d036958b260392f69c2fe9
Mukul Kumar <149408801+mukulkumar8285@users.noreply.github.com>
2024-06-19 11:36:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOption = {
-  origin: ["https://net-flix-full-project-2.vercel.app/","http://localhost:3000"],
+  origin: ["https://net-flix-full-project-2.vercel.app" || "http://localhost:3000"],
   methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
