4d7abdb27ec0822a338e39b5037935ea95204cef
Mukul Kumar <149408801+mukulkumar8285@users.noreply.github.com>
2024-06-20 14:11:17 +0530

    Update Header.js

--- netflix/src/components/Header.js
+++ netflix/src/components/Header.js
@@ -3,40 +3,20 @@ import { IoIosArrowDropdown } from "react-icons/io";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { ApiEndPoint } from "../utils/constant";
-import { useLocation , useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { setUser } from "../redux/userSlice";
-import { GetHeader ,  setToggle } from "../redux/MovieSlice";
+import { setToggle } from "../redux/MovieSlice";
 
 
 const Header = () => {
   const store = useSelector((store) => store.app.user);
   const toggle = useSelector((store) => store.movie.toggle);
- const store = useSelector((store) => store.movie.HeaderShow);
-const [userName , SetUserName] = useState(null);
-
-  const saveToLocalStorage = (key, value) => {
-    localStorage.setItem(key, JSON.stringify(value));
-  };
  
-  useEffect(() => {
-    if (user) {
-    saveToLocalStorage('NameData', user);
-    }
-  }, [!user]);
-  useEffect(() => {
-    const storedUserData = JSON.parse(localStorage.getItem('NameData'));
-    if (storedUserData) {
-      SetUserName(storedUserData.fullName); // Set fullName from localStorage to state
-      console.log("i am runung")
-      console.log(storedUserData?.fullName)
-    }
-  }, []);
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const Location = useLocation();
 
-  
   const logouthandler = async (e) => {
     e.preventDefault();
     try {
@@ -54,8 +34,6 @@ const [userName , SetUserName] = useState(null);
       dispatch(setUser(null));
       navigate("/");
       console.log(res.data);
-      localStorage.removeItem("token");
-      localStorage.removeItem("NameData");
     } catch (error) {
       toast.error(error.response.data.message);
       console.log("logout error", error);
@@ -68,11 +46,6 @@ const [userName , SetUserName] = useState(null);
       navigate("/browse")
     }
   }
-   if(Location.pathname === "/"){
-    dispatch(GetHeader(false));
-  }else{
-    dispatch(GetHeader(true));
-  }
   return (
     <div className="absolute z-10 flex w-[100%] items-center justify-between px-6 bg-gradient-to-b from-black">
       <img
@@ -83,15 +56,15 @@ const [userName , SetUserName] = useState(null);
       {store && (
         <div className="flex items-center">
           <IoIosArrowDropdown size={"24px"} className="text-white" />
-          <h1 className="text-lg font-bold text-white">{userName || user?.fullName}</h1>
+          <h1 className="text-lg font-bold text-white">{store.fullName}</h1>
           <div className="ml-4">
             <button
               onClick={logouthandler}
-              className="bg-red-800 text-white px-4 py-2 rounded-md"
+              className="bg-red-800 text-white px-4 py-2"
             >
               Logout
             </button>
-            <button onClick={SearchToggle} className="bg-red-800 text-white px-4 py-2 ml-2 rounded-md">
+            <button onClick={SearchToggle} className="bg-red-800 text-white px-4 py-2 ml-2">
               {
                 toggle ?  "Home" : "Search Movie" 
               }
